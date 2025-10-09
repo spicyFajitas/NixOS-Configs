95c1ca2db5ea1dc93b42ce91fbb395f255eecafd
spicyFajitas <afulton101222@gmail.com>
2025-10-09 18:22:14 -0400

    udisks2

--- system/services.nix
+++ system/services.nix
@@ -8,5 +8,11 @@
 
   # printing
   services.printing.enable = true;
+
+  # nautilus file browser network browsing
+  services.gvfs.enable = true; # Enables GNOME Virtual File System (includes smb, sftp, etc.)
+  services.avahi.enable = true; # Helps discover network shares via mDNS
+  services.udisks2.enable = true;
+
 }
 
