6d5cd0dbc09dcc7a1d97b777fd9534813c57b6ab
spicyFajitas <afulton101222@gmail.com>
2025-10-09 01:13:42 -0400

    useOSProber

--- system/boot.nix
+++ system/boot.nix
@@ -7,6 +7,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.loader.grub.useOSProber = true;
+
   boot.initrd.luks.devices."luks-c51c4979-0b74-4edd-a41b-c18dde777470".device =
     "/dev/disk/by-uuid/c51c4979-0b74-4edd-a41b-c18dde777470";
 }
