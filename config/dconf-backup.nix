53d8fd8eb51e074735a0789eae7d2ebd9aee1f67
spicyFajitas <afulton101222@gmail.com>
2025-10-24 04:34:30 -0400

    fix script

--- config/dconf-backup.nix
+++ config/dconf-backup.nix
@@ -5,7 +5,7 @@
     text = ''
       #!/bin/bash
       # run `sudo dconf load / < exported-configs/dconf/<file>` to restore dconf config
-      /run/current-system/sw/bin/dconf dump / > /home/adam/Documents/code/NixOS-Configs/exported-configs/dconf/dconf-backup-$(echo $(date +%F-%H-%M-%S))
+      /run/current-system/sw/bin/dconf dump / > /home/adam/Documents/code/nixos-configs/exported-configs/dconf/dconf-backup-$(echo $(date +%F-%H-%M-%S))
     '';
     # Optional: dependencies
     # deps = [ "bash" ]; # list of packages
